7154fc28a86ec5a7c612087508080f1d73e56d75
MikeKrysan <krysanmihasan@gmail.com>
2023-03-21 22:32:03 +0200

    eight commit - create branch Module29_HomeWork

--- app/src/main/java/com/mikekrysan/homework/DetailsFragment.kt
+++ app/src/main/java/com/mikekrysan/homework/DetailsFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.film_item.*
 import kotlinx.android.synthetic.main.fragment_details.*
 import kotlinx.android.synthetic.main.fragment_details.*
 
@@ -60,7 +62,7 @@ class DetailsFragment : Fragment() {
         details_toolbar.title = film.title
         //Устанавливаем картинку
         details_poster.setImageResource(film.poster)
-//            //Устанавливаем описание
+        //Устанавливаем описание
         details_description.text = film.description
 
         details_fab_favorites.setImageResource(
